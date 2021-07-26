e8d195f6eeec406fb4bfb5ebd9754e2e024fb82c
John M. Stassen <john.stassen@gmail.com>
2021-07-26 10:06:32 -0700

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let elementHearts = document.querySelectorAll(".like-glyph");
 
+function clickCallback(e) {
+  let heart = e.target;
+  mimicServerCall()
+    .then(function(){
+      // STEP 2: Uncomment the next 3 lines.
+      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
+      // mimic Pillar 3 (Server Communication) to only update the screen if the
+      // sending of information to the server succeeds.
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart")
+      } else {
+        heart.innerText = EMPTY_HEART  
+        heart.classList.remove("activated-heart")
+      }
+    })
+    .catch(function(error) {
+      let modal = document.querySelector("#modal");
+      modal.classList.remove("hidden")
+      modal.innerHTML = error;
+      window.setTimeout(clearError, 3000)
+    });
+}
 
+function clearError() {
+  modal.classList.add("hidden")
+}
+
+for (let glyph of elementHearts) {
+  glyph.addEventListener("click", clickCallback);
+}
 
 
 //------------------------------------------------------------------------------
